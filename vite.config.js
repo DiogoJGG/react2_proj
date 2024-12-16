5396bc972eec521a1d93ca8f06f4e055eba1e588
DiogoJGG <diogojgg+github@gmail.com>
2024-12-16 17:12:04 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/react2_proj/',
   build: {
     outDir: 'dist',  // A pasta de saída padrão
   },
